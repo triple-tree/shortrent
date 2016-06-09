dafd791a5d5bc1ba12eb053e354217b21a0863c5
liudonghua123 <liudonghua123@gmail.com>
2016-06-09 08:51:21 +0800

    add a standard annotation on repositry

--- src/main/java/com/tripletree/repository/UserRepository.java
+++ src/main/java/com/tripletree/repository/UserRepository.java
@@ -3,10 +3,12 @@ package com.tripletree.repository;
 import com.tripletree.entity.User;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by liudonghua on 2016-06-08.
  */
+@Repository
 public interface UserRepository extends PagingAndSortingRepository<User, Long>, JpaSpecificationExecutor<User> {
 
 }
